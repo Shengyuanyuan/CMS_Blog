be1c96ebdd6bb78b9f6f13c4f26d7722109f3c9b
shengyuanyuan <shengyuanyuan.office@gmail.com>
2022-02-19 00:36:55 +0800

    added public/bg2.jpg

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({posts}) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
-        <title>CMS Blog</title>
+        <title>斜咖啡技術筆記</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid gird-cols-1 lg:grid-cols-12 gap-12">
